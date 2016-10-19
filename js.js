fc926bfe8e2f4c11b70de04ddfe66ec7ec5251da
e24031093 <e24031093@mail.ncku.edu.tw>
2016-10-19 13:19:41 +0800

    123

--- js.js
+++ js.js
@@ -2,3 +2,11 @@ function red()
 {
   document.body.style.backgroundColor = "red";
 }
+function blue()
+{
+  document.body.style.backgroundColor = "blue";
+}
+function green()
+{
+  document.body.style.backgroundColor = "green";
+}
