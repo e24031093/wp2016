f8fb44611548ec5b98aeb6a9282eb8189fab8547
e24031093 <e24031093@mail.ncku.edu.tw>
2016-11-09 16:40:56 +0800

    1

--- do.php
+++ do.php
@@ -1,5 +1,16 @@
+$ vi name.json
+617@luffy:/home/wp2016/user/617/git/wp2016/http(gh-pages 35m)
+$ cd ..
+617@luffy:/home/wp2016/user/617/git/wp2016(gh-pages 36m)
+$ vi index.html
+617@luffy:/home/wp2016/user/617/git/wp2016(gh-pages 38m)
+$ vi do.php
+
+[1]+  Stopped                 vim do.php
+617@luffy:/home/wp2016/user/617/git/wp2016(gh-pages 38m)
+$ vi do.php
 Hello <br>
 <?php
 $name = $_REQUEST['user_name'];
-echo "$name !";
+echo "$name!";
 ?>
